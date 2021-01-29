f9d33eb2366d62113ffcda74ad1014b3b57a6348
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-01-29 17:32:09 +0100

    added filters dropdown

--- src/containers/Filters.tsx
+++ src/containers/Filters.tsx
@@ -1,13 +1,20 @@
+//importing hooks
+import { useState } from 'react';
+//filters component
 const Filters: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  //toggling filters & rotate animation class
+  const isOpenClass = isOpen ? 'active' : '';
+  const rotateClass = isOpen ? 'rotate' : '';
   return (
     <div className="filters__container">
-      <div className="toggle__filters">
+      <div className="toggle__filters" onClick={() => setIsOpen(!isOpen)}>
         <div className="toggle__filters__text">Filter by Region</div>
-        <div className="chevron__right__icon">
+        <div className={`chevron__right__icon ${rotateClass}`}>
           <i className="fas fa-chevron-right"></i>
         </div>
       </div>
-      <div className="regions">
+      <div className={`regions ${isOpenClass}`}>
         <ul className="regions__list">
           <li>Africa</li>
           <li>America</li>
