53fc86d3cb5ecff2c07e84779535d9ac2fb2fca6
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-01-29 15:23:02 +0100

    synching devices

---
+++ src/containers/App.tsx
@@ -0,0 +1,23 @@
+//improting styles
+import '../styles/css/app.css';
+import CountriesList from './CountriesList';
+import Filters from './Filters';
+//importing components
+import Header from './Header';
+import SearchBar from './SearchBar';
+//app component
+const App = () => {
+  return (
+    <div className="app__container">
+      <Header />
+      <div className="wrapper">
+        <div className="flex__container">
+          <SearchBar />
+          <Filters />
+        </div>
+        <CountriesList />
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ src/containers/SearchBar.tsx
@@ -0,0 +1,17 @@
+const SearchBar = () => {
+  return (
+    <div className="search__bar">
+      <div className="search__bar__container">
+        <div className="search__icon">
+          <i className="fas fa-search"></i>
+        </div>
+        <input
+          type="text"
+          placeholder="Search for a country..."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
