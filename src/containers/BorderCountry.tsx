ef8c041c332f027e1c834ff72d249bb36dbfd0d5
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-01-31 17:12:59 +0100

    successfully showing border names

--- src/containers/BorderCountry.tsx
+++ src/containers/BorderCountry.tsx
@@ -1,15 +1,24 @@
-//importing Link & hooks
+//importing Link & hooks & random id's
+import { v4 as uuidv4 } from 'uuid';
 import { Link } from 'react-router-dom';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+//iso alpha 3 to name converter
+import countries from 'i18n-iso-countries';
 //border country component
 const BorderCountry: React.FC = () => {
+  //iniitalizinig the converter with english language
+  countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+  //getting state
   const { error, data } = useTypedSelector(state => state.country);
+  //returning border components
   if (data) {
-    const returnedBorders = data.borders.map((border: string) => {
+    //mapping over 3 borders only
+    const lessBorders = data.borders.slice(0, 3);
+    const returnedBorders = lessBorders.map((border: string) => {
       return (
-        <div className="border__country__container">
+        <div className="border__country__container" key={uuidv4()}>
           <Link to="/details" className="border__country">
-            {border}
+            {countries.getName(border, 'en', { select: 'official' })}
           </Link>
         </div>
       );
